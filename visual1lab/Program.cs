a418e177362deeac65c867be5782fd0209ef89fd
prizemin <78478873+prizemin@users.noreply.github.com>
2022-02-17 22:53:56 +0700

    commit

--- visual1lab/Program.cs
+++ visual1lab/Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Programm
+{
+    static void Main()
+    {
+        int[] queue1 = { 5, 3, 4 };
+        int registers1 = 1;
+        int[] queue2 = { 10, 2, 3, 3 };
+        int registers2 = 2;
+        int[] queue3 = { 2, 3, 10 };
+        int registers3 = 2;
+
+        Console.WriteLine(HW1.QueueTime(queue1, registers1));
+        Console.WriteLine(HW1.QueueTime(queue2, registers2));
+        Console.WriteLine(HW1.QueueTime(queue3, registers3));
+
+    }
+}
+public class HW1
+{
+    public static long QueueTime(int[] customers, int n)
+    {
+        int theTime = 0;
+        while (customers.Length > 0)
+        {
+            int busyRegister = 0;
+
+            if (n > customers.Length)
+            {
+                 busyRegister = customers.Length;
+            }
+            else
+            {
+                busyRegister = n;
+            }
+
+            for (int x = 0; x < busyRegister; x++)
+            {
+                customers[x]--;
+            }
+
+            customers = Array.FindAll(customers, y => y != 0);
+
+            theTime++;
+        }
+        return theTime;
+    }
+}
+
